332c76395aa26cc89b7611b3780f24c83a0976ce
Freddessson <fredriksson.otto@gmail.com>
2016-04-25 12:51:29 +0200

    commit

--- CobraHotel/DAL/RoomDAL.cs
+++ CobraHotel/DAL/RoomDAL.cs
@@ -11,42 +11,6 @@ namespace DAL
 {
     public class RoomDAL
     {
-        public static void CreateRoom(Room r)
-        {
-            DBUtil conn = new DBUtil();
-            SqlConnection myConnection = conn.Connection();
-            string roomId = r.roomId;
-            int price = r.price;
-            string beds = c.email;
-            string phone = c.phone;
-            string address = c.address;
-
-            try
-            {
-                using (myConnection)
-                {
-
-                    string sql = "INSERT INTO dbo.customer (pnr, name, email, phone, address) VALUES (@pnr, @name, @email, @phone, @address)";
-                    SqlCommand cmd = new SqlCommand(sql, myConnection);
-                    cmd.Parameters.Add("@pnr", SqlDbType.VarChar).Value = pnr;
-                    cmd.Parameters.Add("@name", SqlDbType.VarChar, 50).Value = name;
-                    cmd.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = email;
-                    cmd.Parameters.Add("@phone", SqlDbType.VarChar, 50).Value = phone;
-                    cmd.Parameters.Add("@address", SqlDbType.VarChar, 50).Value = address;
-                    cmd.CommandType = CommandType.Text;
-                    cmd.ExecuteNonQuery();
-                }
-
-
-            }
-            catch (SqlException)
-            {
-                //ERROR
-                Console.Write("Kunde inte skapa kund.");
-            }
-            conn.CloseConn(myConnection);
-        }
-
         public static List <Room> GetAvailableRoomsByPeriod(String period)
         {
 
@@ -58,8 +22,11 @@ namespace DAL
             try
             {
                 SqlDataReader myReader = null;
-                SqlCommand cmd = new SqlCommand("SELECT * FROM room WHERE period LIKE @prd", myConnection);
-                cmd.Parameters.Add("@period", SqlDbType.VarChar, 50).Value = period = "y";
+                SqlCommand cmd = new SqlCommand("SELECT * FROM room WHERE available = @available ", myConnection);
+                //period = "y";
+                available = "y";
+                //cmd.Parameters.Add("@period", SqlDbType.VarChar, 50).Value = period;
+                cmd.Parameters.Add("@available", SqlDbType.VarChar, 50).Value = available;
                 myReader = cmd.ExecuteReader();
 
                 while (myReader.Read())
@@ -71,24 +38,21 @@ namespace DAL
                     r.period = myReader["period"].ToString();
                     r.price = Convert.ToInt32(myReader["price"]);
                     r.roomId = myReader["roomId"].ToString();
-                    r.roomNumber = myReader["rooomNumber"].ToString();
+                    r.roomNumber = myReader["roomNumber"].ToString();
 
 
                     roomList.Add(r);
                 }
                 foreach (Room r in roomList)
                 {
-                    Console.WriteLine("Available"+ r.available);
+                    Console.WriteLine("Available: "+ r.available);
                 }
             }
-            catch (SqlException)
+            catch (SqlException e)
             {
 
-                throw;
+                Console.WriteLine(e.Message);
             }
-
-
-
             return roomList;
         }
     }
